fae0f4b60b5f3cf8f915b58e5cc2c010e045ed8f
Vinicius <viniciussilva707@gmail.com>
2025-07-08 18:58:43 -0300

    remoção do standalone para uso traidicional da aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,10 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  // standalone: true, // Removido para uso tradicional
+  // imports: [RouterOutlet], // Removido para uso tradicional
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'front-end-challenge';
